3f897a3cbcd2247d79bbf92319bedd5a2a3bace0
Nandishwar Garg <nandishwar786garg@gmail.com>
2023-07-26 18:11:30 +0530

    Update github.tf

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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" # or `GITHUB_TOKEN`
+  token = "GITHUB_TOKEN"
 }
 
 resource "github_repository" "terraform-learning" {
